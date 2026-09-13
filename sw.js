a8dca4290cf9232100ef28dfe4d616271746d7af
sudeecorp-eng <sudeecorp@gmail.com>
2026-09-13 11:07:09 +0700

    feat: add sw.js

---
+++ sw.js
@@ -0,0 +1,2 @@
+const CACHE='namichii-v1';const ASSETS=['./','./index.html','./style.css','./app.js','./manifest.webmanifest','./icon.svg'];self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));self.addEventListener('activate',e=>e.waitUntil(self.clients.claim()));self.addEventListener('fetch',e=>{if(e.request.method==='GET'&&new URL(e.request.url).origin===location.origin)e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)));});
+
